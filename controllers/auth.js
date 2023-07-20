3b201eb51defcd18d7014fd4bfcd57a6fdb9a445
Jose Hiderman Vargas <hiderman17@gmail.com>
2023-07-20 13:05:50 -0500

    Se modifico el controller de el login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,42 @@
-const {response, request} = require ('express')
-const { generarJWT } = require('../helpers/generar-jwt')
-const Usuario = require ('../models/usuario')
-const bcrypt = require ('bcryptjs')
+const { response, request } = require('express');
+const { generarJWT } = require('../helpers/generar-jwt');
+const Usuario = require('../models/usuario');
+const bcrypt = require('bcryptjs');
 
-const login = async (req,res) => {
-    const {Correo,Contrasena} = req.body
+const login = async (req, res) => {
+    const { Correo, Contrasena } = req.body;
 
     try {
-        // si el correo existe en la bse de datos 
-        // Buscar si el usuario existe en la collection
-        const usuario = await Usuario.findOne({Correo})
+        // Si el correo existe en la base de datos
+        // Buscar si el usuario existe en la colección
+        const usuario = await Usuario.findOne({ Correo });
 
         if (!usuario) {
-            return res.estatus(400).json({
-                msg:'Usuario / correo no encontrado'
-            })
+            return res.status(400).json({
+                msg: 'Usuario / correo no encontrado'
+            });
         }
 
-        if(! bcrypt.compare(usuario.Contrasena == Contrasena)){
+        const contrasenaCorrecta = await bcrypt.compare(Contrasena, usuario.Contrasena);
+        if (!contrasenaCorrecta) {
             return res.status(400).json({
                 msg: 'Usuario / contraseña incorrecta'
-            })    
+            });
         }
-        const token = await generarJWT(Usuario._id)
+
+        const token = await generarJWT(usuario._id);
         res.json({
             token
-        })
-        console.log('SESION INICIADA')
+        });
+        console.log('SESION INICIADA');
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
 
     // Si el password existe en la base de datos
 
-}
+};
 
 module.exports = {
-   login
-}
+    login
+};
