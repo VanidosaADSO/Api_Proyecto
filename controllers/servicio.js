544719a3982ec5a27fbc9cc6baa0ae11b4b3f18d
Jose Hiderman Vargas <hiderman17@gmail.com>
2023-08-08 15:59:26 -0500

    Funcionalidad de la imagen

--- controllers/servicio.js
+++ controllers/servicio.js
@@ -6,37 +6,40 @@ const getservicio = async (req, res) => {
         servicio
     })
 }
+
 const postservicio = async (req, res) => {
     const { Nombre, Tiempo, Precio, Descripcion, Estado } = req.body;
-
-    if (!req.files || !req.files.Imagen) {
-        return res.status(400).json({ message: 'No se ha proporcionado ninguna imagen.' });
+    
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).json({ error: 'No files were uploaded.' });
     }
 
-    const uploadedImage = req.files.Imagen;
-    const servicio1 = new servicios({ Nombre, Tiempo, Precio, Descripcion, Estado });
+    const ImagenFile = req.files.Imagen;
 
-    servicio1.Imagen = uploadedImage.name; 
-    try {
-        await servicio1.save();
+    const uploadPath = __dirname + '/uploads/' + ImagenFile.name;
 
-        const uploadPath = __dirname + '/uploads/' + uploadedImage.name;
-        uploadedImage.mv(uploadPath, async (err) => {
-            if (err) {
-                return res.status(500).json({ message: 'Error al guardar la imagen.', error: err.message });
-            }
+    ImagenFile.mv(uploadPath, async function(err) {
+        if (err) {
+            return res.status(500).json({ error: err.message });
+        }
 
-            // Almacenar la URL de la imagen en la base de datos
-            servicio1.Imagen = '/uploads/' + uploadedImage.name;
-            await servicio1.save();
-
-            res.status(201).json({ message: 'Servicio creado exitosamente.', servicio: servicio1 });
+        const servicio1 = new servicios({
+            Nombre,
+            Tiempo,
+            Precio,
+            Descripcion,
+            Imagen: uploadPath, 
+            Estado
         });
-    } catch (error) {
-        res.status(500).json({ message: 'Error al crear el servicio.', error: error.message });
-    }
-};
 
+        try {
+            await servicio1.save();
+            res.json({ servicio1 });
+        } catch (error) {
+            return res.status(500).json({ error: error.message });
+        }
+    });
+};
 
 const putservicio = async (req, res) => {
     const { _id, Nombre, Tiempo, Precio, Descripcion, Imagen, Estado } = req.body
