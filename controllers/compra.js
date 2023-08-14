6f534826dc1bc7568a1e53548837c55c0d61f990
Jose Hiderman Vargas <hiderman17@gmail.com>
2023-08-14 11:08:37 -0500

    Se modifico el put de compras para que cuando se modifiquen los productos de una compra se vea los cambios de cantidad en los insumos

--- controllers/compra.js
+++ controllers/compra.js
@@ -34,52 +34,70 @@ const putcompras = async (req, res) => {
     const { _id, N_factura, M_pago, Fecha, Proveedor, Productos, Estado } = req.body;
 
     if (Productos && Array.isArray(Productos)) {
+        try {
+            const compra = await compras.findOne({ _id: _id });
 
-        const compra = await compras.findOne({ _id: _id });
-
-        if (!compra) {
-            return res.status(404).json({ error: 'Compra no encontrada' });
-        }
-
-        let updatedTotal = compra.Total_factura;
-
-        const updatedProductos = compra.Productos.map(existingProducto => {
-            const productoData = Productos.find(p => p._id && p._id.toString() === existingProducto._id.toString());
-            if (productoData) {
-                if (productoData.eliminar) {
-                    // Producto marcado para eliminación, resta su contribución al total
-                    updatedTotal -= existingProducto.Cantidad * existingProducto.Precio;
-                    return null; // Marcar para eliminación
-                }
-                const updatedProducto = { ...existingProducto.toObject(), ...productoData };
-                const cambioEnTotal = (updatedProducto.Cantidad * updatedProducto.Precio) - (existingProducto.Cantidad * existingProducto.Precio);
-                updatedTotal += cambioEnTotal;
-                return updatedProducto;
+            if (!compra) {
+                return res.status(404).json({ error: 'Compra no encontrada' });
             }
-            return existingProducto;
-        }).filter(producto => producto !== null); // Filtrar productos marcados para eliminación
 
-        // Agregar nuevos productos en el array de Productos
-        const nuevosProductos = Productos.filter(p => !p._id);
-        nuevosProductos.forEach(nuevoProducto => {
-            updatedProductos.push(nuevoProducto);
-            updatedTotal += nuevoProducto.Cantidad * nuevoProducto.Precio;
-        });
+            let updatedTotal = compra.Total_factura;
 
-        // Realiza la actualización en la base de datos
-        const updatedCompra = await compras.findByIdAndUpdate(
-            { _id: _id },
-            { N_factura, M_pago, Fecha, Proveedor, Productos: updatedProductos, Total_factura: updatedTotal, Estado },
-            { new: true }
-        );
+            const insumosUpdates = []; // Almacenará las actualizaciones de insumos
 
-        res.json({
-            compra: updatedCompra,
-        });
+            const updatedProductos = compra.Productos.map(existingProducto => {
+                const productoData = Productos.find(p => p._id && p._id.toString() === existingProducto._id.toString());
+                if (productoData) {
+                    if (productoData.eliminar) {
+                        // Producto marcado para eliminación, resta su contribución al total
+                        updatedTotal -= existingProducto.Cantidad * existingProducto.Precio;
+                        insumosUpdates.push({ nombre: existingProducto.Nombre, cantidad: -existingProducto.Cantidad });
+                        return null; // Marcar para eliminación
+                    }
+                    const updatedProducto = { ...existingProducto.toObject(), ...productoData };
+                    const cambioEnTotal = (updatedProducto.Cantidad * updatedProducto.Precio) - (existingProducto.Cantidad * existingProducto.Precio);
+                    updatedTotal += cambioEnTotal;
+                    const cantidadDiferencia = updatedProducto.Cantidad - existingProducto.Cantidad;
+                    insumosUpdates.push({ nombre: existingProducto.Nombre, cantidad: cantidadDiferencia });
+                    return updatedProducto;
+                }
+                return existingProducto;
+            }).filter(producto => producto !== null); // Filtrar productos marcados para eliminación
+
+            // Agregar nuevos productos en el array de Productos
+            const nuevosProductos = Productos.filter(p => !p._id);
+            nuevosProductos.forEach(nuevoProducto => {
+                updatedProductos.push(nuevoProducto);
+                updatedTotal += nuevoProducto.Cantidad * nuevoProducto.Precio;
+                insumosUpdates.push({ nombre: nuevoProducto.Nombre, cantidad: nuevoProducto.Cantidad });
+            });
+
+            // Realiza las actualizaciones de cantidad en la colección "insumos"
+            for (const insumoUpdate of insumosUpdates) {
+                await insumos.updateOne(
+                    { Nombre: insumoUpdate.nombre },
+                    { $inc: { Cantidad: insumoUpdate.cantidad } }
+                );
+            }
+
+            // Realiza la actualización en la base de datos de compras
+            const updatedCompra = await compras.findByIdAndUpdate(
+                { _id: _id },
+                { N_factura, M_pago, Fecha, Proveedor, Productos: updatedProductos, Total_factura: updatedTotal, Estado },
+                { new: true }
+            );
+
+            res.json({
+                compra: updatedCompra,
+            });
+        } catch (error) {
+            res.status(500).json({ error: 'Error en el servidor' });
+        }
     } else {
         res.status(400).json({ error: 'La propiedad Productos debe ser un array' });
     }
 };
+
 
 
 const patchcompra = async (req, res) => {
