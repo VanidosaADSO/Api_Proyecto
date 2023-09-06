999c9c1fa8feb5c174781ae86e091bb80e969f7c
Jose Hiderman Vargas <hiderman17@gmail.com>
2023-09-05 23:48:46 -0500

    Restablcer contraseña

--- controllers/resContrasena.js
+++ controllers/resContrasena.js
@@ -5,41 +5,41 @@ const saltRounds = 10;
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-        user: 'AutenticacionVanidosa@gmail.com',
-        pass: 'njxbkzuvwqtbbuca'
-    }
+  service: 'Gmail',
+  auth: {
+    user: 'AutenticacionVanidosa@gmail.com',
+    pass: 'njxbkzuvwqtbbuca'
+  }
 });
 
 const postOlvidocontrasena = async (req, res) => {
-    const { Correo } = req.body;
+  const { Correo } = req.body;
 
-    try {
-        const usuario = await Usuario.findOne({ Correo: Correo });
-        if (!usuario) {
-            return res.status(404).json({ error: 'Usuario no encontrado' });
+  try {
+    const usuario = await Usuario.findOne({ Correo: Correo });
+    if (!usuario) {
+      return res.status(404).json({ error: 'Usuario no encontrado' });
+    }
+
+    const token = await generarJWT(usuario._id);
+
+    usuario.token = token;
+    await usuario.save();
+
+    const resetLink = `https://rescontrasena.onrender.com?token=${token}`;
+
+    const mailOptions = {
+      from: 'AutenticacionVanidosa@gmail.com',
+      to: Correo,
+      subject: 'Recuperar contraseña',
+      attachments: [
+        {
+          filename: 'logo.png',
+          path: __dirname + '/logo.png',
+          cid: 'logo@unique_cid'
         }
-
-        const token = await generarJWT(usuario._id);
-
-        usuario.token = token;
-        await usuario.save();
-
-        const resetLink = `https://rescontrasena.onrender.com?token=${token}`;
-
-        const mailOptions = {
-            from: 'AutenticacionVanidosa@gmail.com',
-            to: Correo,
-            subject: 'Recuperar contraseña',
-            attachments: [
-                {
-                    filename: 'logo.png',
-                    path: __dirname + '/logo.png',
-                    cid: 'logo@unique_cid'
-                }
-            ],
-            html: `<div style="
+      ],
+      html: `<div style="
             font-family: Arial, Helvetica, sans-serif; 
             display: flex;
             justify-content: center;
@@ -73,32 +73,24 @@ const postOlvidocontrasena = async (req, res) => {
                 }
         
                 .logo {
-                  width: 130px !important;
+                  width: 120px !important;
                 }
         
                 .title-name {
-                  font-size: 18px !important;
-                }
-        
-                h3 {
-                  font-size: 17px !important;
-                }
-        
-                h4 {
-                  font-size: 15px !important;
+                  font-size: 1em !important;
                 }
         
                 .text {
-                  font-size: 14px !important;
+                  font-size: 0.75em !important;
                 }
         
                 .reset-button {
                   padding: 9px 15px !important;
-                  font-size: 14px !important;
+                  font-size: 0.75em !important;
                 }
         
                 .text-vanidosa {
-                  font-size: 12px !important;
+                  font-size: 0.625em !important;
                 }
               }
             </style>
@@ -109,8 +101,8 @@ const postOlvidocontrasena = async (req, res) => {
               background-color: #273746; 
             ">
         
-              <div style="display: flex; justify-content: center; margin: 1.5em 0;">
-                <img class="logo" src=".cid:logo@unique_cid" alt="Logo Vanidosa SPA" style="width: 140px; color: #ffffff;">
+              <div style="text-align: center; margin: 1.5em 0;">
+                <img class="logo" src="cid:logo@unique_cid" alt="Logo Vanidosa SPA" style="width: 140px; color: #ffffff;">
               </div>
         
               <h2 Class="title-name" style="text-align: center; font-size: 22px; color: #ffffff;">Hola, ${Correo}</h2>
@@ -119,7 +111,7 @@ const postOlvidocontrasena = async (req, res) => {
                 Haz clic en el siguiente enlace para restablecer tu contraseña:
               </p>
         
-              <div style="margin: 10px 0px 20px 0px; display: flex; justify-content: center;">
+              <div style="margin: 20px 0px; text-align: center;">
         
                 <a class="reset-button" id="resetLink" href="${resetLink}" style="
                   padding: 10px 15px; 
@@ -151,9 +143,9 @@ const postOlvidocontrasena = async (req, res) => {
         
           </div>
             `,
-        };
+    };
 
-        const script = `
+    const script = `
             <script>
                 document.getElementById("resetLink").addEventListener("click", function(event) {
                     event.preventDefault();
@@ -164,50 +156,50 @@ const postOlvidocontrasena = async (req, res) => {
             </script>
         `;
 
-        // Agrega el script JavaScript al final del HTML del correo
-        mailOptions.html += script;
+    // Agrega el script JavaScript al final del HTML del correo
+    mailOptions.html += script;
 
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                console.log(error);
-                res.status(500).json({ error: 'Ocurrió un error al enviar el correo electrónico' });
-            } else {
-                console.log('Correo enviado: ' + info.response);
-                res.json({ message: 'Correo enviado con éxito' });
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
-    }
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Ocurrió un error al enviar el correo electrónico' });
+      } else {
+        console.log('Correo enviado: ' + info.response);
+        res.json({ message: 'Correo enviado con éxito' });
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+  }
 };
 
 const patchContrasena = async (req, res) => {
-    const { Contrasena, token } = req.body;
+  const { Contrasena, token } = req.body;
 
-    try {
-        // Verificar si el token es válido y encontrar al usuario correspondiente
-        const usuario = await Usuario.findOne({ token: token });
-        if (!usuario) {
-            return res.status(401).json({ error: 'Token inválido o usuario no encontrado' });
-        }
-
-        // Generar un nuevo hash para la contraseña
-        const hashedContrasena = await bcrypt.hash(Contrasena, saltRounds);
-
-        // Actualizar la contraseña y eliminar el token una vez que se haya usado
-        usuario.Contrasena = hashedContrasena;
-        usuario.token = null;
-        await usuario.save();
-
-        res.json({ message: 'Contraseña actualizada con éxito' });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+  try {
+    // Verificar si el token es válido y encontrar al usuario correspondiente
+    const usuario = await Usuario.findOne({ token: token });
+    if (!usuario) {
+      return res.status(401).json({ error: 'Token inválido o usuario no encontrado' });
     }
+
+    // Generar un nuevo hash para la contraseña
+    const hashedContrasena = await bcrypt.hash(Contrasena, saltRounds);
+
+    // Actualizar la contraseña y eliminar el token una vez que se haya usado
+    usuario.Contrasena = hashedContrasena;
+    usuario.token = null;
+    await usuario.save();
+
+    res.json({ message: 'Contraseña actualizada con éxito' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Ocurrió un error al procesar la solicitud' });
+  }
 };
 
 module.exports = {
-    postOlvidocontrasena,
-    patchContrasena
+  postOlvidocontrasena,
+  patchContrasena
 };
